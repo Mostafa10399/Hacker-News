0b80bda31876f25360b452ec8a0a49c6d2e820cd
Mostafa10399 <51030209+Mostafa10399@users.noreply.github.com>
2022-02-02 16:12:23 +0200

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Mostafa Mahmoud on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
